86f092da6c101b3c975c434be83655019d9f1d31
Conner R. Phillips <conner.r.phillips@gmail.com>
2017-04-15 09:18:16 +0200

    Delete dfgit.fish, new:dfg.fish

--- .config/fish/functions/dfgit.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function dfgit --wraps 'git' --description 'alias dfgit /usr/bin/git --git-dir=/home/conner/.dfgit --work-tree=/home/conner'
-	/usr/bin/git --git-dir=/home/conner/.dfgit --work-tree=/home/conner $argv;
-end
