06f4b677445d105cf0b0a3135cb17104f81aa0dd
Conner R. Phillips <conner.r.phillips@gmail.com>
2017-04-18 09:54:00 +0200

    Add EDITOR variable to fish.config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 #set -gx PATH /home/conner/anaconda3/bin /usr/bin /usr/local/bin
-
+export EDITOR=vim
+export VISUAL=vim
 set fish_key_bindings fish_vi_key_bindings
 #fish_user_key_bindings
 #set fish_key_bindings fish_user_key_bindings
